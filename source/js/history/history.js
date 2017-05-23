90ef8b8132efe4e73284408ca47bbe6541e24157
Mary Gushchina <m.o.gushchina@gmail.com>
2017-05-23 23:25:34 +0400

    fix history API (#11)
    * history: update API
    
    * history: fix API
    
    * history: fix names, method for generation ID
    
    * history: fix variable name (ids)
    
    * history: fix generate ID method
    
    * history: fix calculating id in the method for generation ID
    
    * history: fix variable 'postfix' initialization in the method for generation ID
    
    * history: fix method for generation ID

--- source/js/history/history.js
+++ source/js/history/history.js
@@ -1,7 +1,19 @@
+/**
+ * @typedef {Object} ControllerOptions
+ * @property {string} id - chat id
+ *
+ * @typedef {Object} ItemHistory
+ * @property {string} id
+ * @property {string} name
+ * @property {ControllerOptions} options
+ * @property {State} state
+ */
+
 class History {
 
   constructor() {
-    this._items = [];
+    this._ids = new Set();
+    this._items = {};
   }
 
   /**
@@ -12,8 +24,33 @@ class History {
    * @returns {Item}
    */
   get current() {
-    const index = this._items.length - 1;
-    return this._items[index];
+    return this._items[this.currentId];
+  }
+
+  get currentId() {
+    const ids = [...this._ids];
+    return ids[this._ids.size - 1];
+  }
+
+  /**
+   * @returns {string}
+   */
+  get currentName() {
+    return this.current ? this.current.id : null;
+  }
+
+  /**
+   * @returns {ItemHistory|null}
+   */
+  backView() {
+    const list = [...this._ids];
+    const prevId = list[this._ids.size - 2];
+    return this._items[prevId] || null;
+  }
+
+  clear() {
+    this._ids.clear();
+    this._items = {};
   }
 
   /**
@@ -24,20 +61,53 @@ class History {
    * @param {Options} options - state options
    */
   pushState(state, options) {
-    const item = {
-      state,
+    const id = this._generateId();
+    const name = state.name;
+    const itemHistory = {
+      id,
+      name,
       options,
+      state,
     };
 
-    if (this.current === item) {
+    if (this.currentName === itemHistory) {
       return;
     }
 
-    this._items.push(item);
+    this._ids.add(id);
+    this._items[id] = itemHistory;
   }
 
   popState() {
-    this._items.pop();
+    const list = [...this._ids];
+    const id = list.pop();
+
+    this._ids.delete(id);
+    delete this._items[id];
+  }
+
+  /**
+   * @returns {Object.<ItemHistory>}
+   */
+  viewHistory() {
+    return this._items;
+  }
+
+  _generateId() {
+    const prefix = 'mg';
+    const ids = [...this._ids];
+    let count = 0;
+
+    while (true) {
+      const isUniqueId = !ids.some(p => p === `${prefix}-${count}`);
+      if (isUniqueId) {
+        break;
+      }
+
+      count += 1;
+    }
+
+    return `${prefix}-${count}`;
   }
 }
 
